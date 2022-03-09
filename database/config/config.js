ce7ec0b0840439a943433f15bd5875171ee94fa5
MartinMura <martinalejandropm@gmail.com>
2022-03-08 23:04:47 -0300

    config.js 1.3 modificado production

--- database/config/config.js
+++ database/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     "database": "heroku_1596162e78197f5",
     "host": "us-cdbr-east-05.cleardb.net",
     "dialect": "mysql",
-    "use_env_variable": "mysql://b6f4b858489799:c7ca5ff8@us-cdbr-east-05.cleardb.net/heroku_1596162e78197f5?reconnect=true"
+    
   },
   "test": {
     "username": "root",
@@ -15,10 +15,11 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": "b6f4b858489799",
+    "password": "c7ca5ff8",
+    "database": "heroku_1596162e78197f5",
+    "host": "us-cdbr-east-05.cleardb.net",
+    "dialect": "mysql",
+    "use_env_variable": "mysql://b6f4b858489799:c7ca5ff8@us-cdbr-east-05.cleardb.net/heroku_1596162e78197f5?reconnect=true"
   }
 }
