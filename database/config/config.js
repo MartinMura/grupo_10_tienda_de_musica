dfc3df730577c773fd3f1bb107c6dc2e67a59d47
MartinMura <martinalejandropm@gmail.com>
2022-03-09 13:58:49 -0300

    config.js database schema

--- database/config/config.js
+++ database/config/config.js
@@ -17,7 +17,7 @@ module.exports = {
     production: {
       username: process.env.DB_USER,
       password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
+      database: process.env.DB_SCHEMA,
       host: process.env.DB_HOST,
       dialect: "mysql"
   }
